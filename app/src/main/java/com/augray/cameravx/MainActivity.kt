7377bfd70a415f2c0607e5d42881312be4cef2f5
vignesh <vigneshr@augray.com>
2019-07-15 12:43:43 +0530

    Updated back Button

--- app/src/main/java/com/augray/cameravx/MainActivity.kt
+++ app/src/main/java/com/augray/cameravx/MainActivity.kt
@@ -4,8 +4,10 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os.bind
 import android.view.View
 import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
 import com.augray.visionx.Cameravx
 import com.augray.visionx.bind
 import kotlinx.android.synthetic.main.activity_main.*
